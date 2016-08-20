92dd4218d551f3dbfef660e107d6e037222bdca8
Kerim Gueney <kerim@gueney.org>
2016-08-20 20:25:52 +0200

    Completely remove old JavaScript code and replace it with Vue.JS equivalent

--- js/main.js
+++ js/main.js
@@ -1,73 +1,92 @@
-/* function setFirstAndLast() {
-    var firstElements, lastElements;
-    if ($(window).width() > 1260) {
-        firstElements = 5;
-        lastElements = 4;
-    } else {
-        firstElements = 4;
-        lastElements = 3;
+Vue.filter('umlaute', function (value) {
+        var valueWithUmlaute = value.replace(/ae/g, 'ä');
+        valueWithUmlaute = value.replace(/oe/g, 'ö');
+        valueWithUmlaute = value.replace(/ue/g, 'ü');
+        return valueWithUmlaute;
     }
-    $("section article:visible").each(function (iter) {
-        if (iter == 0 || (iter + 1) % firstElements == 0) {
-            $(this).addClass("first-in-row");
-        }
-        if (iter > 0 && (iter + 1) % lastElements == 0) {
-            $(this).addClass("last-in-row");
-        }
-    });
-}
-
-function removeFirsAndLast() {
-    $("article.first-in-row").removeClass("first-in-row");
-    $("article.last-in-row").removeClass("last-in-row");
-}
-
-$(document).ready(function () {
-
-    var oldWindowSize;
-    if ($(window).width() >= 960) {
-        setFirstAndLast();
-        oldWindowSize = $(window).width();
-    }
-    $("input").change(function () {
-        var value = $(this).val();
-        $("article.article-category." + value).toggle();
-        removeFirsAndLast();
-        if ($(window).width() >= 960) {
-            setFirstAndLast();
-        }
-    });
-    $(window).resize(function () {
-
-        if ((oldWindowSize > 1259 && $(window).width() <= 1259 && $(window).width() >= 960) ||
-            (oldWindowSize > 960 && oldWindowSize <= 1259 && $(window).width() >= 1260) ||
-            (oldWindowSize < 959 && $(window).width() >= 960)){
-            oldWindowSize = $(window).width();
-            removeFirsAndLast();
-            setFirstAndLast();
-        }
-
-        if ($(window).width() <= 959) {
-            oldWindowSize = $(window).width();
-            removeFirsAndLast();
-        }
-    });
-});
-*/
+);
 
 new Vue({
     el: '#content',
-
     data: {
+        filters: [
+            {filter: 'fleisch', visible: true},
+            {filter: 'obst', visible: true},
+            {filter: 'gemuese', visible: true}
+        ],
         articles: [
-            { image: 'resources/apfel.jpg', title: 'Apfel', category: 'obst', text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo lingula eget dolor. Aenean massa. Cum socils natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', link: '#'},
-            { image: 'resources/aubergine.jpg', title: 'Aubergine', category: 'gemuese', text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo lingula eget dolor. Aenean massa. Cum socils natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', link: '#'},
-            { image: 'resources/banane.jpg', title: 'Banane', category: 'obst', text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo lingula eget dolor. Aenean massa. Cum socils natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', link: '#'},
-            { image: 'resources/brokkoli.jpg', title: 'Brokkoli', category: 'gemuese', text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo lingula eget dolor. Aenean massa. Cum socils natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', link: '#'},
-            { image: 'resources/huhn.jpg', title: 'Huhn', category: 'fleisch', text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo lingula eget dolor. Aenean massa. Cum socils natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', link: '#'},
-            { image: 'resources/orange.jpg', title: 'Orange', category: 'obst', text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo lingula eget dolor. Aenean massa. Cum socils natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', link: '#'},
-            { image: 'resources/rind.jpg', title: 'Rind', category: 'fleisch', text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo lingula eget dolor. Aenean massa. Cum socils natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', link: '#'},
-            { image: 'resources/zucchini.jpg', title: 'Zucchini', category: 'gemuese', text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo lingula eget dolor. Aenean massa. Cum socils natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', link: '#'}
+            {
+                image: 'resources/apfel.jpg',
+                title: 'Apfel',
+                category: 'obst',
+                text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo lingula eget dolor. Aenean massa. Cum socils natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+                link: '#',
+                visible: true
+            },
+            {
+                image: 'resources/aubergine.jpg',
+                title: 'Aubergine',
+                category: 'gemuese',
+                text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo lingula eget dolor. Aenean massa. Cum socils natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+                link: '#',
+                visible: true
+            },
+            {
+                image: 'resources/banane.jpg',
+                title: 'Banane',
+                category: 'obst',
+                text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo lingula eget dolor. Aenean massa. Cum socils natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+                link: '#',
+                visible: true
+            },
+            {
+                image: 'resources/brokkoli.jpg',
+                title: 'Brokkoli',
+                category: 'gemuese',
+                text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo lingula eget dolor. Aenean massa. Cum socils natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+                link: '#',
+                visible: true
+            },
+            {
+                image: 'resources/huhn.jpg',
+                title: 'Huhn',
+                category: 'fleisch',
+                text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo lingula eget dolor. Aenean massa. Cum socils natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+                link: '#',
+                visible: true
+            },
+            {
+                image: 'resources/orange.jpg',
+                title: 'Orange',
+                category: 'obst',
+                text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo lingula eget dolor. Aenean massa. Cum socils natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+                link: '#',
+                visible: true
+            },
+            {
+                image: 'resources/rind.jpg',
+                title: 'Rind',
+                category: 'fleisch',
+                text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo lingula eget dolor. Aenean massa. Cum socils natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+                link: '#',
+                visible: true
+            },
+            {
+                image: 'resources/zucchini.jpg',
+                title: 'Zucchini',
+                category: 'gemuese',
+                text: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo lingula eget dolor. Aenean massa. Cum socils natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+                link: '#',
+                visible: true
+            }
         ]
+    },
+    methods : {
+        updateVisibility: function (filter) {
+            this.articles.forEach(function (article) {
+                if (article.category === filter)
+                article.visible = !article.visible;
+            })
+        }
     }
-})
+});
